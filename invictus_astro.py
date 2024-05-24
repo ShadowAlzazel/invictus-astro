2d991b0c1dc8f7b564170ff89c75025124436e41
ShadowAlzazel <pastenalfredojr@gmail.com>
2024-05-23 20:22:49 -0700

    Add Loading data from files like registries, Created bat for launching game, worked on path/file walking

---
+++ invictus_astro.py
@@ -0,0 +1,8 @@
+import pygame
+import sys
+import asyncio
+
+from src import main
+
+if __name__ == '__main__':
+    main.launch()
--- src/ships/ship.py
+++ src/ships/ship.py
@@ -9,11 +9,19 @@ class ComponentSlot:
 # Different from componenet, this holds/points a component OBJ,
 # Has internal Reload/Logic
 class ShipComponent:
+    def  __init__(self, stats):
+        self._evasion = stats['evasion']
+        self._speed = stats['speed']
+        self._luck = stats['luck']
+        self._stealth = stats['stealth']
+    
+
+# Template, a ship in not christened until it has its minerva
+class ShipHull:
     def  __init__(self):
         pass
-    
-    
+
     
 class Ship:
-    def  __init__(self, hull):
-        pass
+    def  __init__(self, name, hull):
+        self.name = name
